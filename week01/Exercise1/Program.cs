0268f0bb4e2795ff8d70b67c48f4264f02c1a547
danilomayer <danilomayer2020@gmail.com>
2025-01-22 17:29:33 -0300

    Removed greeting line and changed output sentence from My to Your

--- week01/Exercise1/Program.cs
+++ week01/Exercise1/Program.cs
@@ -4,14 +4,12 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise1 Project.");
-
         Console.WriteLine("What is your first name?"); // Prompt the user to enter FIRST NAME
         string fname = Console.ReadLine(); // Store the user input FIRST NAME in a variable
 
         Console.WriteLine("What is your last name?"); // Prompt the user to enter LAST NAME
         string lname = Console.ReadLine(); //Store the user input LAST NAME in a variable
 
-        Console.WriteLine($"My name is {lname}, {fname} {lname}."); // Output a string that concatenates the First and Last names in a James Bond sentence.
+        Console.WriteLine($"Your name is {lname}, {fname} {lname}."); // Output a string that concatenates the First and Last names in a James Bond sentence.
     }
 }
