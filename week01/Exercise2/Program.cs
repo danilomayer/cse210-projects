f45177294609dcf4fa376065322854b19eb38916
danilomayer <danilomayer2020@gmail.com>
2025-01-22 17:55:27 -0300

    Found way to store number input in int var and did all the ifs with a print in each of them

--- week01/Exercise2/Program.cs
+++ week01/Exercise2/Program.cs
@@ -5,5 +5,29 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World! This is the Exercise2 Project.");
+        Console.WriteLine("What is your grade percentage?"); // Prompt the user to enter their grade
+        string input = Console.ReadLine();
+
+        int grade = int.Parse(input);
+        if (grade >= 90)
+        {
+            Console.WriteLine("Letter grade: A");
+        }
+        else if (grade >= 80)
+        {
+            Console.WriteLine("Letter grade: B");
+        }
+        else if (grade >= 70)
+        {
+            Console.WriteLine("Letter grade: C");
+        }
+        else if (grade >= 60)
+        {
+            Console.WriteLine("Letter grade: D");
+        }
+        else
+        {
+            Console.WriteLine("Letter grade: F");
+        }
     }
 }
