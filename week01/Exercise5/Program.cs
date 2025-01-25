19e370c48b7d77f521b0d4ef64bb46476f78e3ab
danilomayer <danilomayer2020@gmail.com>
2025-01-25 12:04:06 -0300

    Created first two functions welcome and name, and in process of creating the favorite number

--- week01/Exercise5/Program.cs
+++ week01/Exercise5/Program.cs
@@ -5,5 +5,26 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World! This is the Exercise5 Project.");
+
+        static void DisplayWelcome()
+        {
+            Console.WriteLine("Welcome to the Program!");
+        }
+        DisplayWelcome();
+
+        static string PromptUserName()
+        {
+            Console.WriteLine("What is your name?"); // Prompt the user to enter their NAME
+            string name = $"Hello {Console.ReadLine()}."; //Store the user input in a variable
+            return name; // Returns the users name
+        }
+        string greeting = PromptUserName();
+        Console.WriteLine(greeting);
+
+        static int PromptUserNumber()
+        {
+            Console.WriteLine("")
+        }
+        PromptUserNumber()
     }
 }
