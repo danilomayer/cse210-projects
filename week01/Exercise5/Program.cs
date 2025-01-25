a23104384a7d6cb17a7b7f48d8c113d7154f1d7b
danilomayer <danilomayer2020@gmail.com>
2025-01-25 13:07:19 -0300

    Squared number function created and display function is under creation

--- week01/Exercise5/Program.cs
+++ week01/Exercise5/Program.cs
@@ -6,21 +6,24 @@ class Program
     {
         Console.WriteLine("Hello World! This is the Exercise5 Project.");
 
+        // Welcome to the program function
         static void DisplayWelcome()
         {
             Console.WriteLine("Welcome to the Program!");
         }
         DisplayWelcome();
 
+        // User name function
         static string PromptUserName()
         {
             Console.WriteLine("What is your name?"); // Prompt the user to enter their NAME
-            string name = $"Hello {Console.ReadLine()}."; //Store the user input in a variable
+            string name = Console.ReadLine(); //Store the user input in a variable
             return name; // Returns the users name
         }
         string greeting = PromptUserName();
         Console.WriteLine(greeting);
 
+        // Favorite number function
         static int PromptUserNumber()
         {
             Console.WriteLine("What is your favorite integer number?"); // Prompt the user to enter their favorite INTEGER NUMBER
@@ -38,6 +41,22 @@ class Program
             return fav_num; // Returns the users number
         }
         // Print to the user their favorite integer
-        Console.WriteLine($"Your favorite number is {PromptUserNumber()}.");
+        int fav_num = PromptUserNumber();
+        Console.WriteLine($"Your favorite number is {fav_num}.");
+
+        // Squared number function
+        static int SquareNumber(int number)
+        {
+            number = number * number;
+            return number;
+        }
+        int number = fav_num;
+        Console.WriteLine($"Your favorite number squared is {SquareNumber(number)}.");
+
+        // Display results function
+        static string DisplayResult(string name, int square)
+        {
+            Console.WriteLine($"{name}, the square of your number is ")
+        }
     }
 }
