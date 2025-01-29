085bcd2d5a1ee7891bcd75bd8a94c130665790d9
danilomayer <danilomayer2020@gmail.com>
2025-01-29 10:43:03 -0300

    Created base for journal program with option to select actions

--- week02/Journal/Program.cs
+++ week02/Journal/Program.cs
@@ -4,6 +4,25 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Journal Project.");
+        Console.WriteLine(@"Please select one of the following choices:
+        1. Write
+        2. Display
+        3. Load
+        4. Save
+        5. Quit
+        What would you like to do? ");
+
+        // Prompt to type a number and call each function
+        WriteEntry();
+    }
+
+    static void WriteEntry()
+    {
+        // Display prompt 
+        // Ask for entry
+        // Store it with date in a list
+        Console.WriteLine("");
+
+
     }
 }
