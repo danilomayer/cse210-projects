5b1daffeed328923aeb1992950e7d0a8e979ad80
danilomayer <danilomayer2020@gmail.com>
2025-01-29 10:45:06 -0300

    Created Job.cs and actions on main program to add new job to list

---
+++ week02/Resumes/Job.cs
@@ -0,0 +1,9 @@
+// Keeps track of the company, job title, start year, and end year.
+public class Job
+{
+    // Displays the job information in the format "Job Title (Company) StartYear-EndYear", for example: "Software Engineer (Microsoft) 2019-2022".
+    public string Company { get; set; }
+    public string _jobTitle;
+    public int _startYear;
+    public int _endYear;
+}
--- week02/Resumes/Program.cs
+++ week02/Resumes/Program.cs
@@ -4,6 +4,22 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Resumes Project.");
+        Job job1 = new Job();
+        job1.Company = "Google";
+        job1._jobTitle = "CEO";
+        job1._startYear = 2001;
+        job1._endYear = 2025;
+        Console.WriteLine(job1.Company);
+
+        /* Resume my_resume = new Resume();
+        my_resume.Name = "Vincent"; */
     }
 }
+
+/* Keeps track of the person's name and a list of their jobs.
+public class Resume
+{
+Displays the resume, which shows the name first, followed by displaying each one of the jobs.
+
+string name;
+} */
