f2bb764e8816448092a297f176986c5f406e292d
danilomayer <danilomayer2020@gmail.com>
2025-01-30 18:54:32 -0300

    Added name and jobs to myResume

--- week02/Resumes/Program.cs
+++ week02/Resumes/Program.cs
@@ -9,16 +9,21 @@ class Program
         job1.JobTitle = "CEO";
         job1.StartYear = 2001;
         job1.EndYear = 2025;
-        job1.Display();
+        //job1.Display();
 
         Job job2 = new Job();
         job2.Company = "Kellogg";
         job2.JobTitle = "Eating Consultant";
         job2.StartYear = 1998;
         job2.EndYear = 2000;
-        job2.Display();
+        //job2.Display();
 
-        /* Resume my_resume = new Resume();
-        my_resume.Name = "Vincent"; */
+        Resume myResume = new Resume();
+        myResume.Name = "Vincent";
+
+        myResume.Jobs.Add(job1);
+        myResume.Jobs.Add(job2);
+
+        Console.WriteLine(myResume.Jobs[0].JobTitle);
     }
 }
