cb6306dc8444cd48c0fdb395a2b8ac0db2a29b20
danilomayer <danilomayer2020@gmail.com>
2025-01-31 19:51:11 -0300

    Ask user to choose an option and created list with prompts

--- week02/Journal/Program.cs
+++ week02/Journal/Program.cs
@@ -13,10 +13,10 @@ class Program
         What would you like to do? ");
 
         // Prompt to type a number and call each function
-        WriteEntry();
+        Entry();
     }
 
-    static void WriteEntry()
+    static void Entry()
     {
         // Display prompt 
         // Ask for entry
@@ -24,5 +24,15 @@ class Program
         Console.WriteLine("");
 
 
-    }
+        private List<string> prompts = new List<string>
+        {
+            "What was the most memorable part of your day?",
+            "Did you face any challenges today? How did you handle them?",
+            "What is something new you learned today?",
+            "Describe a moment that made you smile today.",
+            "If you could relive one part of today, what would it be and why?",
+            "What is one thing you’re grateful for today?",
+            "What goal or task do you want to accomplish tomorrow?"
+        };
+}
 }
