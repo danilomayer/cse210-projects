cf52ca52272a6528b2346ec2e12ed98c5d13682e
danilomayer <danilomayer2020@gmail.com>
2025-02-01 13:47:45 -0300

    Add Fraction class with default constructor and instances

--- week03/Fractions/Program.cs
+++ week03/Fractions/Program.cs
@@ -7,3 +7,20 @@ class Program
         Console.WriteLine("Hello World! This is the Fractions Project.");
     }
 }
+
+class Fraction
+{
+    private int _top;
+    private int _bottom;
+
+    public Fraction()
+    {
+        _top = 1;
+        _bottom = 1;
+    }
+
+}
+
+Fraction f1 = new Fraction();
+Fraction f2 = new Fraction(1, 4);
+Fraction f3 = new Fraction(2, 5);
