8d78707f4531938fff42f7efd23521094d9d4792
danilomayer <danilomayer2020@gmail.com>
2025-02-05 20:37:02 -0300

    added the getdecimalvalue method and correspondent constructors were adjusted

--- week03/Fractions/Program.cs
+++ week03/Fractions/Program.cs
@@ -7,9 +7,20 @@ class Program
         Console.WriteLine("Hello World! This is the Fractions Project.");
 
 
-        Fraction f1 = new Fraction();
-        Fraction f2 = new Fraction(1, 4);
-        Fraction f3 = new Fraction(2, 5);
+        Fraction f1 = new Fraction(1, 1);
+        Fraction f2 = new Fraction(5, 1);
+        Fraction f3 = new Fraction(3, 4);
+        Fraction f4 = new Fraction(1, 3);
+
+        // Display initial values
+        Console.WriteLine($"f1: {f1.GetFractionString()}");
+        Console.WriteLine(f1.GetDecimalValue());
+        Console.WriteLine($"f2: {f2.GetFractionString()}");
+        Console.WriteLine(f2.GetDecimalValue());
+        Console.WriteLine($"f3: {f3.GetFractionString()}");
+        Console.WriteLine(f3.GetDecimalValue());
+        Console.WriteLine($"f4: {f4.GetFractionString()}");
+        Console.WriteLine(f4.GetDecimalValue());
     }
 }
 
@@ -40,5 +51,15 @@ class Fraction
     {
         get { return _bottom; }
         set { _bottom = value; }
+    }
+
+    public string GetFractionString()
+    {
+        return $"{_top}/{_bottom}";
+    }
+
+    public double GetDecimalValue()
+    {
+        return (double)_top / _bottom;
     }
 }
