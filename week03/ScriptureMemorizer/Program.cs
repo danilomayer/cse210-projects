9b9c8cb40992f9a3e664b1fe1f88f0d95c9ee8bf
danilomayer <danilomayer2020@gmail.com>
2025-02-08 22:51:32 -0300

    constructors and getters and setters added to class reference

--- week03/ScriptureMemorizer/Program.cs
+++ week03/ScriptureMemorizer/Program.cs
@@ -12,6 +12,11 @@ class Program
 // Keeps track of both the reference and the text of the scripture. Can hide words and get the rendered display of the text.
 class Scripture
 {
+    // Attributes
+    private string _reference; // The reference of the scripture.
+    private string _words; // The text of the scripture.
+
+    // Class Behaviors
     public void HideRandomWords(int number)
     {
         return;
@@ -31,6 +36,11 @@ class Scripture
 // Keeps track of a single word and whether it is shown or hidden.
 class Word
 {
+    // Attributes
+    private string _text; // The text of the word.
+    private bool _hidden; // Whether the word is hidden.
+
+    // Class Behaviors
     public void Hide()
     {
         return;
@@ -55,8 +65,84 @@ class Word
 // Keeps track of the book, chapter, and verse information.
 class Reference
 {
+    // Attributes
+    private string _book; // The name of the book.
+    private int _chapter; // The chapter number.
+    private int _verse; // The verse number.
+    private int _endVerse; // The ending verse number.
+
+    // Constructor for single verse reference
+    public Reference(string book, int chapter, int verse)
+    {
+        _book = book;
+        _chapter = chapter;
+        _verse = verse;
+        _endVerse = verse;
+    }
+
+    // Constructor for range of verses
+    public Reference(string book, int chapter, int startVerse, int endVerse)
+    {
+        _book = book;
+        _chapter = chapter;
+        _verse = startVerse;
+        _endVerse = endVerse;
+    }
+
+    // Getters and Setters for Book
+    public string GetBook()
+    {
+        return _book;
+    }
+
+    public int SetBook(string book)
+    {
+        _book = book;
+    }
+
+    // Getters and Setters for Chapter
+    public int GetChapter()
+    {
+        return _chapter;
+    }
+
+    public void SetChapter(int chapter)
+    {
+        _chapter = chapter;
+    }
+
+    // Getters and Setters for Verse
+    public int GetVerse()
+    {
+        return _verse;
+    }
+
+    public void SerVerse(int verse)
+    {
+        _verse = verse;
+    }
+
+    // Getters and Setters for EndVerse
+    public int GetEndVerse()
+    {
+        return _endVerse;
+    }
+
+    public void SetEndVerse(int endVerse)
+    {
+        _endVerse = endVerse;
+    }
+
+    // Class Behavior to display text
     public string GetDisplayText()
     {
-        return "";
+        if (_verse == _endVerse)
+        {
+            return $"{_book} {_chapter}:{_verse}";
+        }
+        else
+        {
+            return $"{_book} {_chapter}:{_verse}-{_endVerse}";
+        }
     }
 }
