8c1746fb2142c147c0286f7dd3391ffa139eaca9
danilomayer <danilomayer2020@gmail.com>
2025-02-13 19:49:38 -0300

    Wrote video class with it's methods

--- week04/YouTubeVideos/Program.cs
+++ week04/YouTubeVideos/Program.cs
@@ -4,6 +4,40 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the YouTubeVideos Project.");
+
     }
 }
+
+// Track the title, author, and length (in seconds) of the video. Store a list of comments, and return the number of comments.
+class Video
+{
+    private string title;
+    private string author;
+    private int length;
+    private List<Comment> comments;
+
+    AddComment(Comment comment)
+    {
+        comments.Add(comment);
+    }
+
+    GetCommentCount()
+    {
+        return comments.Count;
+    }
+
+    DisplayInfo()
+    {
+        Console.WriteLine("Title: " + title);
+        Console.WriteLine("Author: " + author);
+        Console.WriteLine("Length: " + length);
+        Console.WriteLine("Comment Count: " + GetCommentCount());
+    }
+}
+
+// Tracking the name of the person who made the comment and the text of the comment.
+class Comment
+{
+
+}
+
