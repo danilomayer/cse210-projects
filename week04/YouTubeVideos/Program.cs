15869d3de044c72dc758b4b630cd97f22eb10259
danilomayer <danilomayer2020@gmail.com>
2025-02-14 12:23:15 -0300

    Comment class with it's attributes and behaviors and fixed video class

--- week04/YouTubeVideos/Program.cs
+++ week04/YouTubeVideos/Program.cs
@@ -13,31 +13,58 @@ class Video
 {
     private string title;
     private string author;
-    private int length;
-    private List<Comment> comments;
+    private int lengthInSeconds;
+    private List<Comment> comments = new List<Comment>();
 
-    AddComment(Comment comment)
+    public Video(string title, string author, int lengthInSeconds)
+    {
+        this.title = title;
+        this.author = author;
+        this.lengthInSeconds = lengthInSeconds;
+    }
+
+    public void AddComment(Comment comment)
     {
         comments.Add(comment);
     }
 
-    GetCommentCount()
+    public int GetCommentCount()
     {
         return comments.Count;
     }
 
-    DisplayInfo()
+    public void DisplayVideoDetails()
     {
         Console.WriteLine("Title: " + title);
         Console.WriteLine("Author: " + author);
-        Console.WriteLine("Length: " + length);
-        Console.WriteLine("Comment Count: " + GetCommentCount());
+        Console.WriteLine("Length: " + lengthInSeconds + " seconds");
+        Console.WriteLine("Number of Comments: " + GetCommentCount());
+        Console.WriteLine("Comments:");
+
+        foreach (var comment in comments)
+        {
+            Console.WriteLine(" - " + comment.GetCommentDetails());
+        }
+
+        Console.WriteLine();
     }
 }
 
 // Tracking the name of the person who made the comment and the text of the comment.
 class Comment
 {
+    private string commenterName;
+    private string text;
 
+    public Comment(string commenterName, string text)
+    {
+        this.commenterName = commenterName;
+        this.text = text;
+    }
+
+    public string GetCommentDetails()
+    {
+        return commenterName + ": " + text;
+    }
 }
 
