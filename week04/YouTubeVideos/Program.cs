16ba47d2faf91ee681f9db0e139119093759d3f7
danilomayer <danilomayer2020@gmail.com>
2025-02-14 12:35:14 -0300

    update class program to create instances for the video class and display info to user

--- week04/YouTubeVideos/Program.cs
+++ week04/YouTubeVideos/Program.cs
@@ -4,7 +4,34 @@ class Program
 {
     static void Main(string[] args)
     {
+        // Create a new video
+        Video video1 = new Video("C# Tutorial", "Bob", 60);
+        Video video2 = new Video("Python Tutorial", "Alice", 120);
+        Video video3 = new Video("Java Tutorial", "Charlie", 180);
 
+        // Add comments video 1
+        video1.AddComment(new Comment("John", "Great video!"));
+        video1.AddComment(new Comment("Jane", "I learned a lot from this video!"));
+        video1.AddComment(new Comment("Jim", "This video is very helpful!"));
+
+        // Add comments video 2
+        video2.AddComment(new Comment("Mike", "Fantastic video!"));
+        video2.AddComment(new Comment("Molly", "I love this video!"));
+        video2.AddComment(new Comment("Max", "This video is amazing!"));
+        video2.AddComment(new Comment("Mia", "I learned a lot from this video!"));
+
+        // Add comments video 3
+        video3.AddComment(new Comment("Nathan", "Marvelous video!"));
+        video3.AddComment(new Comment("Nina", "I learned a lot from this video!"));
+
+        // Store videos in a List
+        List<Video> videos = new List<Video> { video1, video2, video3 };
+
+        // Display video details
+        foreach (var video in videos)
+        {
+            video.DisplayVideoDetails();
+        }
     }
 }
 
