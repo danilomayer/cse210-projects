2eb88df3fceb00645a705a270b43850a8e4162c7
danilomayer <danilomayer2020@gmail.com>
2025-02-14 18:39:11 -0300

    writing the class address with variables and behaviors

--- week04/OnlineOrdering/Program.cs
+++ week04/OnlineOrdering/Program.cs
@@ -3,7 +3,32 @@ using System;
 class Program
 {
     static void Main(string[] args)
+    { }
+}
+
+class Address
+{
+    private string _street;
+    private string _city;
+    private string _state;
+    private string _country;
+
+    public Address(string street, string city, string state, string country)
     {
-        Console.WriteLine("Hello World! This is the OnlineOrdering Project.");
+        _street = street;
+        _city = city;
+        _state = state;
+        _country = country;
     }
+
+    public bool IsInUSA()
+    {
+        return _country.ToLower() == "usa";
+    }
+
+    public string GetFullAddress()
+    {
+        return _street + "\n" + _city + ", " + _state + "\n" + _country;
+    }
+
 }
