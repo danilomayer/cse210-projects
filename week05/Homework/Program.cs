f77741a8f72d7b5e758cfd197b8632933c159a3d
danilomayer <danilomayer2020@gmail.com>
2025-02-15 17:47:45 -0300

    Create separate file for the assignment class and the children classes for the specific assignments

--- week05/Homework/Program.cs
+++ week05/Homework/Program.cs
@@ -4,6 +4,28 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Homework Project.");
+
+    }
+}
+
+class MathAssignment : Assignment
+{
+    private string _textbookSection;
+    private string _problems;
+
+    public string GetHomeworkList()
+    {
+
+    }
+
+}
+
+class WritingAssignment : Assignment
+{
+    private string _title;
+
+    public string GetHomeworkList()
+    {
+
     }
 }
