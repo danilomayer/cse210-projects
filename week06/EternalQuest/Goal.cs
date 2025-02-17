e660c8c3f084fbf813f1920ee574dcc7f162e7eb
danilomayer <danilomayer2020@gmail.com>
2025-02-17 19:44:13 -0300

    Goal class built with classes and constructors planned

---
+++ week06/EternalQuest/Goal.cs
@@ -0,0 +1,62 @@
+using System;
+
+class Goal
+{
+    private string _shortName;
+    private string _description;
+    private string _points;
+
+    public Goal(string shortName, string description, string points)
+    {
+        _shortName = shortName;
+        _description = description;
+        _points = points;
+    }
+
+    // Getters and Setters
+    public string GetShortName()
+    {
+        return _shortName;
+    }
+    public void SetShortName(string shortName)
+    {
+        _shortName = shortName;
+    }
+
+    public string GetDescription()
+    {
+        return _description;
+    }
+    public void SetDescription(string description)
+    {
+        _description = description;
+    }
+
+    public string GetPoints()
+    {
+        return _points;
+    }
+    public void SetPoints(string points)
+    {
+        _points = points;
+    }
+
+    // Methods
+    public void RecordEvent()
+    { }
+
+    public bool IsComplete()
+    {
+        return false;
+    }
+
+    public string GetDetailString()
+    {
+        return "";
+    }
+
+    public string GetStringRepresentation()
+    {
+        return "";
+    }
+}
