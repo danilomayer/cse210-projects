3d4d4490e6f3988322cc041129a50fa06dc4cf47
danilomayer <danilomayer2020@gmail.com>
2025-02-17 14:41:46 -0300

    Reflecting activity class done with lists of prompts and method to get random prompts and questions

--- week05/Mindfulness/ReflectingActivity.cs
+++ week05/Mindfulness/ReflectingActivity.cs
@@ -1,30 +1,66 @@
 using System;
+using System.Collections.Generic;
 
-class ReflectingActivity
+class ReflectingActivity : Activity
 {
     private List<string> _prompts;
     private List<string> _questions;
-    public ReflectingActivity()
+
+    public ReflectingActivity() : base("Reflecting Activity", "This activity will help you reflect on times in your life when you have shown strength and resilience. This will help you recognize the power you have and how you can use it in other aspects of your life.")
     {
-        _prompts = new List<string>();
-        _questions = new List<string>();
+        _prompts = new List<string>
+        {
+            "Think of a time when you stood up for someone else.",
+            "Think of a time when you did something really difficult.",
+            "Think of a time when you helped someone in need.",
+            "Think of a time when you did something truly selfless."
+        };
+
+        _questions = new List<string>
+        {
+            "Why was this experience meaningful to you?",
+            "Have you ever done anything like this before?",
+            "How did you get started?",
+            "How did you feel when it was complete?",
+            "What made this time different than other times when you were not as successful?",
+            "What is your favorite thing about this experience?",
+            "What could you learn from this experience that applies to other situations?",
+            "What did you learn about yourself through this experience?",
+            "How can you keep this experience in mind in the future?"
+        };
     }
+
     public void Run()
-    { }
-    public string GetRandomPrompt()
     {
-        return "";
-    }
-    public string GetRandomQuestion()
-    {
-        return "";
-    }
-    public void DisplayPrompt()
-    {
+        DisplayStartingMessage();
         Console.WriteLine(GetRandomPrompt());
+        int duration = GetDuration();
+        while (duration > 0)
+        {
+            Console.WriteLine(GetRandomQuestion());
+            ShowSpinner(5);
+            duration -= 5;
+        }
+        DisplayEndingMessage();
     }
-    public void DisplayQuestion()
+
+    private string GetRandomPrompt()
     {
-        Console.WriteLine(GetRandomQuestion());
+        Random random = new Random();
+        int index = random.Next(_prompts.Count);
+        return _prompts[index];
+    }
+
+    private string GetRandomQuestion()
+    {
+        Random random = new Random();
+        int index = random.Next(_questions.Count);
+        return _questions[index];
+    }
+
+    private int GetDuration()
+    {
+        // Assuming duration is set in DisplayStartingMessage
+        return _duration;
     }
 }
