d66710900a9e243438611205298153698f05bedb
danilomayer <danilomayer2020@gmail.com>
2025-02-17 14:36:49 -0300

    breathing activity class finished displaying count for exercise and durations

--- week05/Mindfulness/BreathingActivity.cs
+++ week05/Mindfulness/BreathingActivity.cs
@@ -1,7 +1,30 @@
 using System;
 
-class BreathingActivity
+class BreathingActivity : Activity
 {
-    public BreathingActivity()
+    public BreathingActivity() : base("Breathing Activity", "This activity will help you relax by walking you through breathing in and out slowly. Clear your mind and focus on your breathing.")
     { }
+
+    public void Run()
+    {
+        DisplayStartingMessage();
+        int duration = GetDuration();
+        while (duration > 0)
+        {
+            Console.WriteLine("Breathe in...");
+            ShowCountDown(5);
+            duration -= 5;
+
+            Console.WriteLine("Breathe out...");
+            ShowCountDown(5);
+            duration -= 5;
+        }
+        DisplayEndingMessage();
+    }
+
+    private int GetDuration()
+    {
+        // Assuming duration is set in DisplayStartingMessage
+        return _duration;
+    }
 }
