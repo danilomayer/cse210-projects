be0719cb1c2408bb7f6adcd5b1d93430372b321e
danilomayer <danilomayer2020@gmail.com>
2025-02-17 14:34:58 -0300

    Built the activity class according to instructions to make the program work

--- week05/Mindfulness/Activity.cs
+++ week05/Mindfulness/Activity.cs
@@ -6,27 +6,50 @@ class Activity
     private string _description;
     private int _duration;
 
-    public Activity(string name, string description, int duration)
+    public Activity(string name, string description)
     {
         _name = name;
         _description = description;
+    }
+
+    public void SetDuration(int duration)
+    {
         _duration = duration;
     }
 
     public void DisplayStartingMessage()
     {
         Console.WriteLine($"Starting {_name}...");
+        Console.WriteLine(_description);
+        Console.WriteLine("How many seconds would you like for this activity?");
+        _duration = int.Parse(Console.ReadLine());
+        Console.WriteLine("Prepare to begin...");
+        ShowSpinner(3);
     }
+
     public void DisplayEndingMessage()
     {
-        Console.WriteLine($"Ending {_name}...");
+        Console.WriteLine("Well done!");
+        Console.WriteLine($"You have completed {_name} for {_duration} seconds.");
+        ShowSpinner(3);
     }
+
     public void ShowSpinner(int seconds)
     {
-
+        for (int i = 0; i < seconds; i++)
+        {
+            Console.Write(".");
+            System.Threading.Thread.Sleep(1000);
+        }
+        Console.WriteLine();
     }
+
     public void ShowCountDown(int seconds)
     {
-
+        for (int i = seconds; i > 0; i--)
+        {
+            Console.WriteLine(i);
+            System.Threading.Thread.Sleep(1000);
+        }
     }
 }
