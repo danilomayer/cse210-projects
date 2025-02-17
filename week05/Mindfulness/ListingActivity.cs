9fbc5ff107bac087d174421323ddefb68af0c5cc
danilomayer <danilomayer2020@gmail.com>
2025-02-17 14:44:49 -0300

    Listing activity generating the prompts for questions and lists

--- week05/Mindfulness/ListingActivity.cs
+++ week05/Mindfulness/ListingActivity.cs
@@ -1,20 +1,52 @@
 using System;
+using System.Collections.Generic;
 
-class ListingActivity
+class ListingActivity : Activity
 {
-    private int _count;
     private List<string> _prompts;
-    public ListingActivity()
+
+    public ListingActivity() : base("Listing Activity", "This activity will help you reflect on the good things in your life by having you list as many things as you can in a certain area.")
     {
-        _count = 0;
-        _prompts = new List<string>();
+        _prompts = new List<string>
+        {
+            "Who are people that you appreciate?",
+            "What are personal strengths of yours?",
+            "Who are people that you have helped this week?",
+            "When have you felt the Holy Ghost this month?",
+            "Who are some of your personal heroes?"
+        };
     }
+
     public void Run()
-    { }
-    public void GetRandomPrompt()
-    { }
-    public List<string> GetListFromUser()
     {
-        return new List<string>();
+        DisplayStartingMessage();
+        Console.WriteLine(GetRandomPrompt());
+        ShowCountDown(5);
+        List<string> userList = GetListFromUser();
+        Console.WriteLine($"You listed {userList.Count} items.");
+        DisplayEndingMessage();
+    }
+
+    private string GetRandomPrompt()
+    {
+        Random random = new Random();
+        int index = random.Next(_prompts.Count);
+        return _prompts[index];
+    }
+
+    private List<string> GetListFromUser()
+    {
+        List<string> userList = new List<string>();
+        Console.WriteLine("Start listing items:");
+        DateTime endTime = DateTime.Now.AddSeconds(_duration);
+        while (DateTime.Now < endTime)
+        {
+            string item = Console.ReadLine();
+            if (!string.IsNullOrEmpty(item))
+            {
+                userList.Add(item);
+            }
+        }
+        return userList;
     }
 }
