ca1c57f6ca9c3114486f110bf456bad638d21291
danilomayer <danilomayer2020@gmail.com>
2025-02-19 10:10:32 -0300

    don't know what I did

--- week05/Homework/MathAssignment.cs
+++ week05/Homework/MathAssignment.cs
@@ -5,7 +5,7 @@ class MathAssignment : Assignment
     private string _textbookSection;
     private string _problems;
 
-    public MathAssignment(string studentName, string topic, string textbookSection, string problems) : base(studentName, topic)
+    public MathAssignment(string studentName, string topic, string _textbookSection, string _problems) : base(studentName, topic)
     {
         _textbookSection = textbookSection;
         _problems = problems;
--- week05/Homework/Program.cs
+++ week05/Homework/Program.cs
@@ -3,17 +3,7 @@ using System;
 class Program
 {
     static void Main(string[] args)
-    {
-        // Assignment object
-        Assignment assignment1 = new Assignment("John", "Math");
-        Console.WriteLine(assignment1.GetSummary());
-
-        // MathAssignment object
-        MathAssignment mathAssignment1 = new MathAssignment("John", "Math", "Chapter 1", "1-10");
-        Console.WriteLine(mathAssignment1.GetHomeworkList());
-
-        // WritingAssignment object
-        WritingAssignment writingAssignment1 = new WritingAssignment("Mary Waters", "European History", "The Causes of World War II by Mary Waters");
-        Console.WriteLine(writingAssignment1.GetWritingInformation());
-    }
+    { }
+    Assignment assignment1 = new Assignment("John", "Math");
+    Console.WriteLine(assignment1.GetSummary());
 }
