10da31172caa5986932a8c87cd01ab48f78a7696
danilomayer <danilomayer2020@gmail.com>
2025-02-20 10:27:17 -0300

    Stationary bicycles class also inherit from activity, tracking speed, minutes and calculating pace

--- week07/ExerciseTracking/StationaryBicycles.cs
+++ week07/ExerciseTracking/StationaryBicycles.cs
@@ -1,6 +1,26 @@
 using System;
 
-class StationaryBicycles
+class StationaryBicycles : Activity
 {
-    private int _speed;
+    private double _speed;
+
+    public StationaryBicycles(DateTime date, int minutes, double speed) : base(date, minutes)
+    {
+        _speed = speed;
+    }
+
+    public override double GetDistance()
+    {
+        return (_speed * _minutes) / 60;
+    }
+
+    public override double GetSpeed()
+    {
+        return _speed;
+    }
+
+    public override double GetPace()
+    {
+        return 60 / _speed;
+    }
 }
