de042da87b0544f8cc7b8d36e0597ae4efce8fb9
danilomayer <danilomayer2020@gmail.com>
2025-02-20 10:25:49 -0300

    running class inherits from activity and tracks distance, speed and pace

--- week07/ExerciseTracking/Running.cs
+++ week07/ExerciseTracking/Running.cs
@@ -1,6 +1,27 @@
 using System;
 
-class Running
+class Running : Activity
 {
     private int _running;
+    private double _distance;
+
+    public Running(DateTime date, int minutes, double distance) : base(date, minutes)
+    {
+        _distance = distance;
+    }
+
+    public override double GetDistance()
+    {
+        return _distance;
+    }
+
+    public override double GetSpeed()
+    {
+        return (_distance / _minutes) * 60;
+    }
+
+    public override double GetPace()
+    {
+        return _minutes / _distance;
+    }
 }
