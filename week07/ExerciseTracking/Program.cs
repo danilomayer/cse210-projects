6c9752581eec492888dd4b18a73ce3828407f842
danilomayer <danilomayer2020@gmail.com>
2025-02-20 10:29:01 -0300

    main program class initiates the program and loops through each activity to print a summary to the user

--- week07/ExerciseTracking/Program.cs
+++ week07/ExerciseTracking/Program.cs
@@ -1,9 +1,20 @@
 using System;
+using System.Collections.Generic;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the ExerciseTracking Project.");
+        List<Activity> activities = new List<Activity>
+        {
+            new Running(new DateTime(2022, 11, 3), 30, 4.8),
+            new StationaryBicycles(new DateTime(2022, 11, 3), 30, 20),
+            new Swimming(new DateTime(2022, 11, 3), 30, 40)
+        };
+
+        foreach (var activity in activities)
+        {
+            Console.WriteLine(activity.GetSummary());
+        }
     }
 }
